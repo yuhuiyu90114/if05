e59669395255713f3337f48fb8d1d53949dea9a8
hyyu <hyyu713@yahoo.com.tw>
2021-12-31 21:07:51 +0800

    update file

--- if05.py
+++ if05.py
@@ -1,12 +1,15 @@
 pwd = 'a123456'
 i =3
-while True:
+while i>0:
     user = input ('請輸入密碼: ')
+    i =i-1
     if user == pwd :
         print ('登入成功')
         break
     else:
-        i =i-1
-        print('密碼錯誤! 還有' ,i,'次機會! ')
-        if i== 0:
-             break
+        
+        print('密碼錯誤! ')
+        if i> 0:
+            print(' 還有' ,i,'次機會! ')
+        else:
+            print(' 沒有機會! ')
